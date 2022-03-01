618735dd3ae67ab73c4a121cc141cf3f4c702cab
Vedaaaaant <74095543+Vedaaaaant@users.noreply.github.com>
2022-03-01 11:53:49 +0530

    Added solution - LeetHub

---
+++ Rotate a Linked List - GFG/rotate-a-linked-list.cpp
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        Node* temp = head;
+        while(temp->next)
+        {
+            temp = temp->next;
+        }
+        temp->next = head;
+        Node* end ;
+        while(k-- && head != NULL)
+        {
+            end = head;
+            head = head->next;
+        }
+        end->next= NULL;
+        return head;
+    }
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        Solution ob;
+        head = ob.rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+  // } Driver Code Ends
