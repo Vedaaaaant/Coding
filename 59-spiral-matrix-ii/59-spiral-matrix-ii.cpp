0177cb1dbcc3b71f747924affc432827a384efb8
Vedaaaaant <74095543+Vedaaaaant@users.noreply.github.com>
2022-04-14 00:09:09 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (85.87%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n)
     {
-        int dir=1,i;
+        int i;
         int k=1;
         int left = 0;
         int right = n-1;
@@ -13,51 +13,37 @@ public:
         
         while(left<=right && top<=bottom)
         {
-
-        if(dir == 1)
-        {
-            for(i=left;i<=right;i++){
+            for(i=left;i<=right;i++)
+            {
                 temp[top][i]=k;
                 k++;
             }   
-            dir=2;
             top++;
-        }
-        else if(dir == 2)
-        {
             for(i=top;i<=bottom;i++)
-                {
-                    temp[i][right]=k;
-                    k++;
-                }
-                dir = 3;
-                right--;
-        }
-        else if(dir == 3)
-        {
-            for(i=right;i>=left;i--)
             {
-            temp[bottom][i]=k;
+                temp[i][right]=k;
                 k++;
             }
-            dir = 4;
+            right--;
+            for(i=right;i>=left;i--)
+            {
+                temp[bottom][i]=k;
+                k++;
+            }
             bottom--;
-        }
-        else if(dir == 4)
-        {
             for(i=bottom;i>=top;i--)
             {
                 temp[i][left]=k;
                 k++;
             }
-            dir =1;
             left++;
         }
-    }
         vector<int> ans;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        { 
             ans.clear();
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++)
+            {
                 ans.push_back(temp[i][j]);
             }
             fin.push_back(ans);
