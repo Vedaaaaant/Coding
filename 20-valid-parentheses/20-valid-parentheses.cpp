d309ddbe817eeb26afe10b830781dc6e7649f7b2
Vedaaaaant <74095543+Vedaaaaant@users.noreply.github.com>
2022-05-24 16:11:39 +0530

    Time: 6 ms (10.47%), Space: 6.5 MB (10.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     bool matching(char a , char b)
     {
-        return ((a=='(' && b==')') || (a=='[' && b==']') || (a=='{' && b=='}'));
+        return ((a=='(' && b == ')') || (a=='{' && b == '}') || (a=='[' && b==']')); 
     }
     bool isValid(string s)
     {
         stack<int> st ;
         for(int i = 0 ; i < s.length() ; i++)
         {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
             {
                 st.push(s[i]);
             }
             else
             {
-                if(st.empty()==true)
+                if(st.empty())
                 {
                     return false;
                 }
                 else if(matching(st.top(),s[i])==false)
                 {
-                    return false;
+                    return false ;
                 }
                 else
                 {
@@ -29,6 +29,6 @@ public:
                 }
             }
         }
-        return st.empty();
+        return (st.empty()==true);
     }
 };
