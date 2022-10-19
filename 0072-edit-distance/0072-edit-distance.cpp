887325aab776fbeeb9936bf26b6bb8720e8ac088
Vedaaaaant <74095543+Vedaaaaant@users.noreply.github.com>
2022-10-19 22:09:00 +0530

    Time: 37 ms (32.60%), Space: 9.1 MB (23.62%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minDistance(string str1, string str2) 
+    {
+      int n = str1.size();
+      int m = str2.size();
+      vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+      for(int j = 0 ; j <=m ; j++)  // Base case : if length of str1 == 0 
+      {
+        dp[0][j] = j;
+      }
+      for(int i = 1 ; i <=n ; i++)  // Base case : if length of the str2==0
+      {
+        dp[i][0] = i ;
+      }
+      
+      for(int i = 1 ; i <= n ; i++)
+      {
+        for(int j = 1 ; j <= m ; j++)
+        {
+          if(str1[i-1] == str2[j-1])
+          {
+            dp[i][j] = 0 + dp[i-1][j-1];
+          }
+          else
+          {
+            dp[i][j] = 1 + min(dp[i-1][j] , min(dp[i-1][j-1],dp[i][j-1]));
+          }
+        }
+      }
+      return dp[n][m];
+    }
+};
