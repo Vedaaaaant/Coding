fd09d4bd9846c289aa4127a68aac984200727d80
Vedaaaaant <74095543+Vedaaaaant@users.noreply.github.com>
2022-10-20 22:34:13 +0530

    Time: 30 ms (10.90%), Space: 15.1 MB (25.23%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -7,12 +7,12 @@ public:
       dp[n][0] = dp[n][1] = 0;  // 0 means buy 1 means sell
       int profit = 0 ;
       for(int ind= n-1; ind>=0; ind--)
-      {
+      { // as in recursion we start from n = 1 so in dp we start from backside;
             for(int buy=0; buy<=1; buy++)
             {
                 if(buy==0){// We can buy the stock
-                    profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
-                }
+                    profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]); 
+                }// we have taken negative sign as to find profit we do sell - buy
 
                 if(buy==1){// We can sell the stock
                     profit = max(0+dp[ind+1][1], Arr[ind] + dp[ind+1][0]);
@@ -21,6 +21,6 @@ public:
                 dp[ind][buy]  = profit;
             }
         }
-        return dp[0][0];
+        return dp[0][0]; // answer will be stored in the first index only
     }
 };
