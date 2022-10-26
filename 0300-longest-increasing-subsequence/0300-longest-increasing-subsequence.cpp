82b5efb193329dceca1693cc58bcbc3ab1819497
Vedaaaaant <74095543+Vedaaaaant@users.noreply.github.com>
2022-10-26 12:18:27 +0530

    Time: 353 ms (63.05%), Space: 10.4 MB (71.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) 
-    {// Base case will be ind == n , but we aren't writing as we have initialized dp with 0
-      int n  = nums.size();
-      vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-      for(int ind = n-1 ; ind>=0 ; ind--)
-      {
-        for(int prev_ind = ind-1 ; prev_ind>=-1 ; prev_ind--)
-        {
-          int notTake = 0 + dp[ind+1][prev_ind+1];
-          int Take = 0;
-          if(prev_ind == -1 || nums[ind] > nums[prev_ind])
-          {
-            Take =  1 + dp[ind+1][ind+1]; // we match than we have to update the previous index
-          }
-          dp[ind][prev_ind+1] = max(Take,notTake);
-        }
-      }
-      return dp[0][0];
-    } // we have added one to the prev_ind as we are doing coordinate shifting
+    int lengthOfLIS(vector<int>& nums)
+    {
+       int n = nums.size();
+       vector<int> dp(n,1);
+       int maxi = 1 ; 
+       for(int i = 0 ; i <n ; i++)
+       {
+         for(int prev = 0 ; prev < i ; prev++)
+         {
+           if(nums[prev] < nums[i])
+           {
+             dp[i] = max(dp[i],1+dp[prev]);
+           }
+         }
+         maxi = max(maxi,dp[i]);
+       }
+      return maxi ;
+    }
 };
-// we have initialized 2D matrix of index and prev_index to maintain the record of previous index
