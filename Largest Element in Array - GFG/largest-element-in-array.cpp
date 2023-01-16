1fd29fbd2e7e5a55ef09d73acaa1d1928c79ed48
Vedaaaaant <74095543+Vedaaaaant@users.noreply.github.com>
2023-01-16 21:35:03 +0530

    Added solution - LeetHub

---
+++ Largest Element in Array - GFG/largest-element-in-array.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &nums, int n)
+    {
+        int res = 0 ;
+        for(int i = 1 ; i < n ; i++)
+        {
+            if(nums[i] > nums[res])
+            {
+                res = i ;
+            }
+        }
+        return nums[res];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.largest(arr, n) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
